61d2b0d9f5eb85abe8e96b471cf029fe6fc91d33
zilinzhu <zilinzhu@tencent.com>
2021-01-21 17:25:29 +0800

    startup code for tutorial 1

--- SwiftUI2048/Shared/ContentView.swift
+++ SwiftUI2048/Shared/ContentView.swift
@@ -8,58 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var model = Model()
-
     var body: some View {
-        ZStack {
-            backgroundColor
-                .ignoresSafeArea()
-            VStack {
-                Text("2048")
-                    .font(.system(size: 50))
-                    .fontWeight(.heavy)
-                    .foregroundColor(textColor)
-                    .padding(.top)
-                GameBoard(model: model)
-                    .padding()
-            }
-            Spacer()
-        }
-        .gesture(DragGesture()
-                    .onEnded(dragGesture))
-    }
-    
-    func dragGesture(value: DragGesture.Value) {
-
-        let x = value.translation.width
-        let y = value.translation.height
-        let threshold: CGFloat = 20
-        guard abs(x) > threshold || abs(y) > threshold else {
-            return
-        }
-        
-        var direction: Direction
-        if abs(x) / abs(y) >= 1 {
-            if x > 0 {
-                direction = .right
-            } else {
-                direction = .left
-            }
-        } else {
-            if y > 0 {
-                direction = .down
-            } else {
-                direction = .up
-            }
-        }
-        withAnimation {
-            model.slide(to: direction)
-        }
+        Text("Hello world!")
+            .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(model: Model())
+        ContentView()
     }
 }
